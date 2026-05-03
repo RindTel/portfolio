157f4723256dfaaf17add61c2abdf5e6f7430425
RindTel <dititel@email.com>
2026-05-03 19:08:50 +0200

    resend fix

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,10 +1,10 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
   try {
     const { name, email, subject, message } = await req.json();
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
 
     await resend.emails.send({
       from: 'Portfolio <onboarding@resend.dev>',
