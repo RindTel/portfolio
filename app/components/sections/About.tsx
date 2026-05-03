fe42819a47934f0708ade6392b951ca9f6b22424
RindTel <dititel@email.com>
2026-05-03 16:26:49 +0200

    fix framer motion ease type

--- app/components/sections/About.tsx
+++ app/components/sections/About.tsx
@@ -31,7 +31,7 @@ const itemVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.4, ease: [0.23, 1, 0.32, 1] },
+    transition: { duration: 0.4, ease: [0.25, 0.1, 0.25, 1] as const},
   },
 };
 
